733e82b1fe21803c3c6904e01d9f7656b84c256e
bhadawi404 <abadawi911@gmail.com>
2024-09-14 09:13:27 +0700

    modul base : Amenities model

---
+++ inntouch_base/__manifest__.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+{
+    'name': 'Inntouch Base',
+    'category': 'Inntouch/base',
+    'summary': 'Inntouch Base',
+    'version': '1.0',
+    'description': """Inntouch Base""",
+    'author': "Inntouch",
+    'depends': [],
+    "data": [
+        'views/inntouch_amenities_views.xml',
+        'views/menu_items.xml',
+        'security/ir.model.access.csv'
+    ],
+    'installable': True,
+    'auto_install': False,
+    'application': True,
+}
---
+++ inntouch_base/models/inntouch_amenities.py
@@ -0,0 +1,13 @@
+from odoo import _, api, fields, models
+
+class InntouchAmenities(models.Model):
+    _name = 'inntouch.amenities'
+    _description = 'Amenities'
+
+    name = fields.Char('Amenities Name')
+    is_available = fields.Boolean('Is Available ?', default=True)
+    last_maintenance_date = fields.Date('Last Maintenance')
+    description = fields.Text('Description')
+
+
+    
