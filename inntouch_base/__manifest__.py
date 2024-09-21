cd9d37ba6be3eb35ba338ac12cef0939171a4f78
bhadawi404 <abadawi911@gmail.com>
2024-09-21 11:52:48 +0700

    ketinggalan

--- inntouch_base/__manifest__.py
+++ inntouch_base/__manifest__.py
@@ -8,13 +8,13 @@
     'author': "Inntouch",
     'depends': ["sale_stock", "account"],
     "data": [
+        "views/inntouch_service_type_views.xml",
         "views/inntouch_property_views.xml",
         "views/inntouch_facilities_views.xml",
         "views/inntouch_amenities_views.xml",
         "views/menu_items.xml",
         "security/ir.model.access.csv",
-        "views/inntouch_facilities_views.xml",
-        "views/inntouch_property_views.xml"
+        
     ],
     'installable': True,
     'auto_install': False,
